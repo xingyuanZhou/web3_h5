2ae49274cd516dc6aaa4ae1a6f958bb1718781fc
袁兴杰 <19851090728@163.com>
2025-03-04 21:05:58 +0800

    我的矿机、流动性池样式完成

---
+++ src/components/Vip/Vip.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Progress } from 'vant';
+</script>
+
+<template>
+  <div class="vip">
+    <div class="vip-info">
+      <div class="info-head">
+        <p class="title">矿机当前等级</p>
+        <img class="grade" src="../../assets/images/home/icon_12.png" alt="">
+      </div>
+      <div class="info-bottom">
+        <div class="exp">
+          <p class="title">下一个等级<span>V2</span></p>
+          <p class="num"><span>14920</span>/30000</p>
+        </div>
+        <Progress class="progress" :percentage="50" pivot-text="" stroke-width="8" color="#FFE5D4" track-color="#332F2C"></Progress>
+<!--        <van-progress :percentage="50" stroke-width="8" />-->
+      </div>
+    </div>
+    <img class="logo" src="../../assets/images/home/icon_11.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+@import "./Vip.css";
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Vip from '@/components/Vip/Vip.vue'
+
 import {ref} from 'vue';
 const active = ref(0);
 
@@ -8,7 +10,7 @@ const active = ref(0);
     <van-tabs v-model:active="active" background="transparent" title-inactive-color="#8C7769" title-active-color="#FFD8BF" color="#FFD8BF" :shrink="true" style="width: 100%;">
       <van-tab title="实时行情">
         <div class="home-market">
-          <div class="market-trend card">
+          <div class="trend card">
             <div class="market-info">
               <div class="info-l">
                 <p class="title">
@@ -42,28 +44,28 @@ const active = ref(0);
             </div>
             <div class="market-chart"></div>
           </div>
-          <div class="market-cell card">
+          <div class="cell card">
             <p class="title">
               <img src="../../assets/images/home/icon_04.png" alt="">
               全网总算力
             </p>
             <p class="num">3,738,402.92</p>
           </div>
-          <div class="market-cell card">
+          <div class="cell card">
             <p class="title">
               <img src="../../assets/images/home/icon_05.png" alt="">
               用户算力
             </p>
             <p class="num">2,402.92</p>
           </div>
-          <div class="market-cell card">
+          <div class="cell card">
             <p class="title">
               <img src="../../assets/images/home/icon_06.png" alt="">
               今日产量
             </p>
             <p class="num">82,329.12</p>
           </div>
-          <div class="market-stat card">
+          <div class="stat card">
             <img src="../../assets/images/home/icon_07.png" alt="">
             <p class="num">6,341,983,702.09</p>
             <div class="column">
@@ -84,15 +86,157 @@ const active = ref(0);
             </div>
             <p class="tips">*分红池每日释放1%，根据创世(超级)的业绩权重占比，分配对应红利。</p>
           </div>
-          <div class="market-stat card">
+          <div class="stat card">
             <img src="../../assets/images/home/icon_08.png" alt="">
             <p class="num">6,341,983,702.09</p>
             <p class="tips">*为了您的资产安全，10%的用户资产从swap里回购ETSC并转入该池。</p>
           </div>
         </div>
       </van-tab>
-      <van-tab title="我的矿机"></van-tab>
-      <van-tab title="流动性池"></van-tab>
+      <van-tab title="我的矿机">
+        <div class="home-machine">
+          <Vip/>
+          <div class="table">
+            <p class="table-title">单体算力加速表</p>
+            <div class="table-wrapper">
+              <div class="table-label">
+                <p class="align-l">矿机型号</p>
+                <p>金额</p>
+                <p>预计产矿值</p>
+                <p class="align-r">单体算力</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">Speed 1</p>
+                <p>500FGC</p>
+                <p>1500</p>
+                <p class="align-r">0.4%/日</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">Speed 1</p>
+                <p>500FGC</p>
+                <p>1500</p>
+                <p class="align-r">0.4%/日</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">Speed 1</p>
+                <p>500FGC</p>
+                <p>1500</p>
+                <p class="align-r">0.4%/日</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">Speed 1</p>
+                <p>500FGC</p>
+                <p>1500</p>
+                <p class="align-r">0.4%/日</p>
+              </div>
+            </div>
+          </div>
+          <div class="table">
+            <p class="table-title">链接等级表</p>
+            <div class="table-wrapper">
+              <div class="table-label">
+                <p class="align-l">链接等级</p>
+                <p>链接奖励</p>
+                <p>链下矿机</p>
+                <p class="align-r">链接总量</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">V1</p>
+                <p>0%</p>
+                <p>--</p>
+                <p class="align-r">--</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">V2</p>
+                <p>0%</p>
+                <p>3个V1</p>
+                <p class="align-r">3万</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">V2</p>
+                <p>0%</p>
+                <p>3个V1</p>
+                <p class="align-r">3万</p>
+              </div>
+              <div class="table-item">
+                <p class="align-l">V2</p>
+                <p>0%</p>
+                <p>3个V1</p>
+                <p class="align-r">3万</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </van-tab>
+      <van-tab title="流动性池">
+        <div class="home-jackpot">
+          <Vip/>
+          <div class="cell">
+            <div class="cell-head">
+              <p class="title">ETS数量</p>
+              <p class="num">70%U进入ESWAP</p>
+            </div>
+            <div class="cell-main">
+              <div class="title">ETS数量</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+              </div>
+            </div>
+            <div class="cell-main">
+              <div class="title">EUSD 数量</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+                <p class="des">≈82,329.12 USDT</p>
+              </div>
+            </div>
+          </div>
+          <div class="cell">
+            <div class="cell-head">
+              <p class="title">市值基金</p>
+              <p class="num">10%U进入市值基金</p>
+            </div>
+            <div class="cell-main">
+              <div class="title">EUSD 数量</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+                <p class="des">≈82,329.12 USDT</p>
+              </div>
+            </div>
+          </div>
+          <div class="cell">
+            <div class="cell-head">
+              <p class="title">ETSC回购池</p>
+              <p class="num">10%U购买ETSC</p>
+            </div>
+            <div class="cell-main">
+              <div class="title">EUSD 数量</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+                <p class="des">≈82,329.12 USDT</p>
+              </div>
+            </div>
+          </div>
+          <div class="cell">
+            <div class="cell-head">
+              <p class="title">分红池</p>
+              <p class="num">10%节点团队红利池</p>
+            </div>
+            <div class="cell-main">
+              <div class="title">创世分红(USDT)</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+              </div>
+            </div>
+            <div class="cell-main">
+              <div class="title">超级分红(USDT)</div>
+              <div class="main-box">
+                <p class="num">3,738,402.92</p>
+                <p class="des">≈82,329.12 USDT</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
